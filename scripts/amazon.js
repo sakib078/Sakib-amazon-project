09748fc757a5d1396e35518815fccf9b22e943ef
sakib078 <103163866+sakib078@users.noreply.github.com>
2024-05-27 01:59:25 -0400

    added class messege to added-to-cart & made the cart-quentity dynamic while adding any item to the cart.

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -9,7 +9,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-name limit-text-to-2-lines">
-          ${product.Name}
+          ${product.name}
         </div>
 
         <div class="product-rating-container">
@@ -23,8 +23,8 @@ products.forEach((product) => {
           $${(product.priceCents / 100).toFixed(2)}
         </div>
 
-        <div class="product-quantity-container">
-          <select>
+        <div class="product-quantity-container js-product-quantity-container">
+          <select class="quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -40,7 +40,7 @@ products.forEach((product) => {
 
         <div class="product-spacer"></div>
 
-        <div class="added-to-cart">
+        <div class="added-to-cart js-added-to-cart-${product.id}">
           <img src="images/icons/checkmark.png">
           Added
         </div>
@@ -65,6 +65,12 @@ document.querySelectorAll(".js-add-to-cart")
       const productId = button.dataset.productId;
 
       let matchingItem;
+      let cart_quantity = 0;
+
+
+      let option = document.querySelector(`.quantity-selector-${productId}`);
+
+      let product_quantity = Number(option.value);
 
       cart.forEach((item) => {
         if (productId === item.productId) {
@@ -74,20 +80,51 @@ document.querySelectorAll(".js-add-to-cart")
       })
 
       if (matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += product_quantity;
         console.log("Hello2");
       }
       else {
         cart.push(
           {
             productId: productId,
-            quantity: 1
+            quantity: product_quantity
           }
         );
         console.log("Hello3");
       }
+
+      cart.forEach((item) => {
+        cart_quantity += item.quantity;
+      })
+
+      document.querySelector(".js-cart-quality")
+        .innerHTML = cart_quantity;
       console.log(cart);
+      console.log(cart_quantity);
+
+
+      cart.forEach((item) => {
+        if (productId === item.productId || matchingItem) {
+          let added_to_cart = document.querySelector(`.js-added-to-cart-${productId}`);
+          added_to_cart.classList.add("added-to-cart");
+          added_to_cart.style.opacity = 1;
+          setTimeout(() => {
+            added_to_cart.classList.remove("added-to-cart");
+            added_to_cart.style.opacity = 0;
+          }, 1500);
+
+        }
+      })
 
     })
   })
+
+
+
+// let quantity_selector = selectContainer.querySelector(".quantity-selector");
+
+// console.log(`qs: ${quantity_selector.innerHTML}`);
+
+
+
 
